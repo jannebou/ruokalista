446de60eaf84ea5bdfe6b4eb34d61acefd58d175
Jannebou <janne.sainio15@gmail.com>
2024-10-26 13:24:14 +0300

    1.0

--- scripts/main.gd
+++ scripts/main.gd
@@ -11,9 +11,11 @@ const KAMPUS : String = "https://www.sodexo.fi/ruokalistat/output/weekly_json/10
 @export var frami_b : Button
 @export var kampus_b : Button
 @export var lang_b : Button
+@export var scroll : ScrollContainer
 
-#var ruokalista : Dictionary
 var tänään_on : Dictionary
+var valittu_puoli : String
+static var language : String = "FI"
 
 var viikonpäivät : Array[String] = [
 	"Maanantai",
@@ -26,34 +28,40 @@ var viikonpäivät : Array[String] = [
 ]
 
 
+
 func _ready() -> void:
 	tänään_on = Time.get_datetime_dict_from_system()
 	
 	frami_b.button_pressed = true
-	
 	frami_b.pressed.connect(hae_ja_tee_lista.bind(FRAMI))
 	kampus_b.pressed.connect(hae_ja_tee_lista.bind(KAMPUS))
+	
 	lang_b.pressed.connect(func() -> void:
 		if lang_b.text == "FI":
-			Settings.language = "EN"
+			language = "EN"
 			lang_b.text = "EN"
 		else:
-			Settings.language = "FI"
+			language = "FI"
 			lang_b.text = "FI"
 			
-		hae_ja_tee_lista(FRAMI)
+		lang_b.release_focus()
+		hae_ja_tee_lista(valittu_puoli)
 			)
 	
 	hae_ja_tee_lista(FRAMI)
 
 
-	
+## Haetaan data ja tehdään lista
 func hae_ja_tee_lista(puoli : String) -> void:
 	match puoli:
 		FRAMI:
 			kampus_b.button_pressed = false
+			frami_b.grab_focus()
+			valittu_puoli = FRAMI
 		KAMPUS:
+			kampus_b.grab_focus()
 			frami_b.button_pressed = false
+			valittu_puoli = KAMPUS
 			
 	tyhjää_lista()
 	
@@ -64,25 +72,34 @@ func hae_ja_tee_lista(puoli : String) -> void:
 	header.text = "Ruokalista %s" % ruokalista.timeperiod
 	
 	tee_lista(ruokalista)
-
+	
+	
+## tyhjätään lista
+var auki_oleva_lista : String = ""
 func tyhjää_lista() -> void:
-	for i : Control in get_tree().get_nodes_in_group("Lista"):
+	auki_oleva_lista = ""
+	for i : Lista in get_tree().get_nodes_in_group("Lista"):
+		if i.lista_auki:
+			auki_oleva_lista = i.päivä
 		i.queue_free()
 
-
+## rakennetaan ruokalista
 func tee_lista(ruokalista : Dictionary) -> void:
 	var id : int = 0
+	
 	for päivän_lista in ruokalista.mealdates:
 		var _ruokalista : Lista = lista.instantiate()
-
 		
 		#listan nimi, helpottaa debug
 		_ruokalista.add_to_group("Lista")
 		_ruokalista.name = päivän_lista.date
+		_ruokalista.close_other.connect(sulje_listat)
 		
 		# Avataan tämän päivän lista 
-		if päivän_lista.date == viikonpäivät[tänään_on.weekday - 1]:
+		if päivän_lista.date == viikonpäivät[tänään_on.weekday - 1] or\
+		auki_oleva_lista == päivän_lista.date:
 			_ruokalista.setup(päivän_lista, true)
+			#scroll.ensure_control_visible(_ruokalista)
 		else:
 			_ruokalista.setup(päivän_lista, false)
 		
@@ -92,8 +109,22 @@ func tee_lista(ruokalista : Dictionary) -> void:
 
 		id += 1
 	
-
-
+	
+## Suljetaan muut kuin auki jäävä lista§
+func sulje_listat(auki_jäävä : String) -> void:
+	for list : Lista in get_tree().get_nodes_in_group("Lista"):
+		if list.päivä == auki_jäävä:
+			await get_tree().create_timer(0.05).timeout
+			#match list.päivä:
+				#"Maanantai":
+					#pass
+				#"Perjantai":
+					#scroll.scroll_vertical = 593
+			scroll.ensure_control_visible(list)
+		else:	
+			list.muuta_ruokalistan_näkyvyys(0)
+	
+## haetaan ruokalista sodexon palvelimelta
 var vastaus : Dictionary
 func hae_ruokalista(url : String) -> Dictionary:
 	var http : HTTPRequest = HTTPRequest.new()
--- scripts/pv_lista.gd
+++ scripts/pv_lista.gd
@@ -7,82 +7,163 @@ class_name Lista
 @export var visibility_list : Array[Control]
 
 @export var pääruoka_l : Label
+@export var pääruoka_diet_l : Label
 @export var vegaani_l : Label
+@export var vegaani_diet_l : Label
 @export var keitto_l : Label
+@export var keitto_diet_l : Label
 @export var patonki_l : Label
+@export var patonki_diet_l : Label
 @export var jälkiruoka_l : Label
+@export var jälkiruoka_diet_l : Label
 @export var lisätieto_l : RichTextLabel
+@export var hinta_l : Label
 
 var lisätiedot : Dictionary
+var lista_auki : bool = true
+var päivä : String
+
+signal close_other()
 
 func _ready() -> void:
 	muuta_ruokalistan_näkyvyys()
 		
-	header_b.pressed.connect(muuta_ruokalistan_näkyvyys)
+	header_b.pressed.connect(
+		func():
+			muuta_ruokalistan_näkyvyys()
+			close_other.emit(lisätiedot.date))
+	
 	
 	lisätiedot_b.pressed.connect(näytä_lisätiedot)
 
 func setup(lista : Dictionary, open : bool) -> void:
-	#TODO onko frami vai kampus
-	#header_b.text = "%s \n %s" %  [lista.date, lista.courses["2"].price]
+	päivä = lista.date
 	header_b.text = "%s"  %  lista.date
+	
+	hinta_l.text = lista.courses["2"].price
 	
 	lisätiedot = lista
 	
-	match Settings.language:
+	match Main.language:
 		"FI":
 			if lista.courses.has("1"):
 				vegaani_l.set_text(lista.courses["1"].title_fi)
+				if lista.courses["1"].dietcodes != "":
+					vegaani_diet_l.show()
+					vegaani_diet_l.text = lista.courses["1"].dietcodes
+				
 				
 			if lista.courses.has("2"):
 				pääruoka_l.set_text(lista.courses["2"].title_fi)
+				if lista.courses["2"].dietcodes != "":
+					pääruoka_diet_l.show()
+					pääruoka_diet_l.text = lista.courses["2"].dietcodes
+				
 
 			if lista.courses.has("3"):
 				keitto_l.set_text(lista.courses["3"].title_fi)
+				if lista.courses["3"].dietcodes != "":
+					keitto_diet_l.show()
+					keitto_diet_l.text = lista.courses["3"].dietcodes
 			
 			if lista.courses.has("4"):
 				patonki_l.set_text(lista.courses["4"].title_fi)
+				if lista.courses["4"].dietcodes != "":
+					patonki_diet_l.show()
+					patonki_diet_l.text = lista.courses["4"].dietcodes
 				
 			if lista.courses.has("5"):
 				jälkiruoka_l.set_text(lista.courses["5"].title_fi)
+				if lista.courses["5"].dietcodes != "":
+					jälkiruoka_diet_l.show()
+					jälkiruoka_diet_l.text = lista.courses["5"].dietcodes
 			
-				
-
+			# jos listasta löytyy "6", on kampus valittuna
+			if lista.courses.has("6"):
+				patonki_l.set_text(lista.courses["5"].title_fi)
+				if lista.courses["5"].dietcodes != "":
+					patonki_diet_l.show()
+					patonki_diet_l.text = lista.courses["5"].dietcodes
+					
+				jälkiruoka_l.set_text(lista.courses["6"].title_fi)
+				if lista.courses["6"].dietcodes != "":
+					jälkiruoka_diet_l.show()
+					jälkiruoka_diet_l.text = lista.courses["6"].dietcodes
+					
 		"EN":
 			if lista.courses.has("1"):
 				vegaani_l.set_text(lista.courses["1"].title_en)
+			if lista.courses["1"].dietcodes != "":
+					vegaani_diet_l.show()
+					vegaani_diet_l.text = lista.courses["1"].dietcodes
+				
 				
 			if lista.courses.has("2"):
 				pääruoka_l.set_text(lista.courses["2"].title_en)
+				if lista.courses["2"].dietcodes != "":
+					pääruoka_diet_l.show()
+					pääruoka_diet_l.text = lista.courses["2"].dietcodes
+				
 
 			if lista.courses.has("3"):
 				keitto_l.set_text(lista.courses["3"].title_en)
-				
+				if lista.courses["3"].dietcodes != "":
+					keitto_diet_l.show()
+					keitto_diet_l.text = lista.courses["3"].dietcodes
+			
 			if lista.courses.has("4"):
 				patonki_l.set_text(lista.courses["4"].title_en)
+				if lista.courses["4"].dietcodes != "":
+					patonki_diet_l.show()
+					patonki_diet_l.text = lista.courses["4"].dietcodes
 				
 			if lista.courses.has("5"):
 				jälkiruoka_l.set_text(lista.courses["5"].title_en)
+				if lista.courses["5"].dietcodes != "":
+					jälkiruoka_diet_l.show()
+					jälkiruoka_diet_l.text = lista.courses["5"].dietcodes
 			
+			# jos listasta löytyy "6", on kampus valittuna
+			if lista.courses.has("6"):
+				patonki_l.set_text(lista.courses["5"].title_en)
+				if lista.courses["5"].dietcodes != "":
+					patonki_diet_l.show()
+					patonki_diet_l.text = lista.courses["5"].dietcodes
+					
+				jälkiruoka_l.set_text(lista.courses["6"].title_en)
+				if lista.courses["6"].dietcodes != "":
+					jälkiruoka_diet_l.show()
+					jälkiruoka_diet_l.text = lista.courses["6"].dietcodes
+
 	if open:
 		muuta_ruokalistan_näkyvyys()
 	
 func näytä_lisätiedot() -> void:
 	if lisätieto_l.text != "":
 		lisätieto_l.text = ""
+		
 	else:
 		for i in lisätiedot.courses:
 			if lisätiedot.courses[i].additionalDietInfo.has("allergens"):
-				lisätieto_l.text += "[b]"+lisätiedot.courses[i].title_fi+"[/b]" + "\n"
-				lisätieto_l.text += lisätiedot.courses[i].additionalDietInfo.allergens+"\n"
+				lisätieto_l.text += "[b]%s[/b]%s" % [lisätiedot.courses[i].title_fi, "\n"] 
+				lisätieto_l.text += "	%s%s" %[lisätiedot.courses[i].additionalDietInfo.allergens, "\n"]
 
 
-
-	
-func muuta_ruokalistan_näkyvyys() -> void:
+func muuta_ruokalistan_näkyvyys(s : int = -1) -> void:
 	if header_b.is_inside_tree():
 		header_b.release_focus()
-		
-	for i : Control in visibility_list:
-		i.visible = !i.visible
-	
+	match s:
+		-1: # TOGGLE
+			for i : Control in visibility_list:
+				i.visible = !i.visible
+			lista_auki = !lista_auki
+			
+		0: # FALSE
+			for i : Control in visibility_list:
+				i.visible = false
+			lista_auki = false
+			
+		1: # TRUE
+			for i : Control in visibility_list:
+				i.visible = true
+			lista_auki = true
